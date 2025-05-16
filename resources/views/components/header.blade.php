0b30ad9060feaee31d5494d2ca1d0916a3fe48ff
Phạm Quốc Hưng <146099528+Hungpq13@users.noreply.github.com>
2025-05-17 01:37:20 +0700

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giới Thiệu - StepUp</title>
-    <link rel="stylesheet" href="{{ asset('styles.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+   <link rel="stylesheet" href="https://stepupenglisgh.onrender.com/styles.css">
+    <script src="https://stepupenglisgh.onrender.com/js/popup.js"></script>
+
 
 </head>
 <script>
